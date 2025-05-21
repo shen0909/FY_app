d1afa0b50a890eedab25d3b6bba0f80db902849f
shen <1571817467@qq.com>
2025-05-21 12:54:44 +0800

    feature: 调整首页 图标

--- lib/styles/image_resource.dart
+++ lib/styles/image_resource.dart
@@ -3,11 +3,11 @@ class FYImages {
   static const String appIcon_32 = 'assets/images/appIcon_32.png';
   static const String riskyBg = 'assets/images/risky_bg.png';
   static const String riskIcon = 'assets/images/risky_icon.png';
-  static const String hotIcon = 'assets/images/hot_icon.png';
+  static const String hotIcon = 'assets/images/hot_icon1.png';
   static const String aiIcon = 'assets/images/ai_icon.png';
-  static const String orderIcon = 'assets/images/order_icon.png';
+  static const String orderIcon = 'assets/images/order_icon1.png';
   static const String settingIcon = 'assets/images/setting_icon.png';
-  static const String detailList = 'assets/images/detail_list.png';
+  static const String detailList = 'assets/images/detail_list1.png';
   static const String login_bg = 'assets/images/login_bg.png';
   static const String login_account = 'assets/images/login_account.png';
   static const String login_pwd = 'assets/images/login_pwd.png';
